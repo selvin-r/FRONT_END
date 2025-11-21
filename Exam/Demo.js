4dadcaceb8a58e51c6e6346817df2c7be5ebba0c
SELVIN R <steelselvin2002@gmail.com>
2025-11-21 14:20:28 +0530

    first commit

--- Exam/Demo.js
+++ Exam/Demo.js
@@ -1,13 +1,27 @@
-function foo1() {
-  return {
-    bar: "hello"
-  };
+// function foo1() {
+//   return {
+//     bar: "hello"
+//   };
+// }
+
+// function foo2() {
+//   return  
+//   {  // ← PROBLEM (automatic semicolon insertion){
+//     bar: "hello"
+//   };
+// }
+// console.log(foo1(),foo2())
+
+
+
+function max(nums){
+
+  const maxx = nums.reduce((acc,curr) => acc  > curr ? acc : curr);
+
+  return maxx;
+
 }
 
-function foo2() {
-  return  
-  {  // ← PROBLEM (automatic semicolon insertion){
-    bar: "hello"
-  };
-}
-console.log(foo1(),foo2())
+const nums =[10,20,30,40,50];
+
+console.log(max(nums))
